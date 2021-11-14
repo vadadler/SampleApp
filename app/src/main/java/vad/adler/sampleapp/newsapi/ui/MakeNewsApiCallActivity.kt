c4979817df10e70aced6f010cf2046ec05d12f18
Vad <vad.adler@gmail.com>
2021-11-13 20:18:09 -0800

    work in progress

--- app/src/main/java/vad/adler/sampleapp/newsapi/ui/MakeNewsApiCallActivity.kt
+++ app/src/main/java/vad/adler/sampleapp/newsapi/ui/MakeNewsApiCallActivity.kt
@@ -2,26 +2,32 @@ package vad.adler.sampleapp.newsapi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import vad.adler.sampleapp.MyApplication
 import vad.adler.sampleapp.R
 import javax.inject.Inject
+import vad.adler.sampleapp.databinding.MakeNewsApiCallActivityBinding
 
 class MakeNewsApiCallActivity : AppCompatActivity() {
     // Dagger to provide an instance of MakeNewsApiCallViewModel from the graph.
     @Inject
     lateinit var newsAPIViewModel: MakeNewsApiCallViewModel
 
+    private lateinit var binding: MakeNewsApiCallActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
         // Make Dagger instantiate @Inject fields in MakeNewsApiCall activity.
         (application as MyApplication).appComponent.inject(this)
         // Now MakeNewsApiCallViewModel is available.
 
-        setContentView(R.layout.make_news_api_call_activity)
+        DataBindingUtil.setContentView(this, R.layout.make_news_api_call_activity)
+//        setContentView(this, R.layout.activity_main3)
 
         //newsAPIViewModel.getLatestNews("Tesla", "publishedAt")
 
-        super.onCreate(savedInstanceState)
+
 
     }
 }
