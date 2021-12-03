fa31399523d985a393051831bd5f5f6ea0355a43
Vad <vad.adler@gmail.com>
2021-12-02 18:14:36 -0800

    Continue work on adapter and viewholder.

--- app/src/main/java/vad/adler/sampleapp/newsapi/view/ui/adapter/NewsListAdapter.kt
+++ app/src/main/java/vad/adler/sampleapp/newsapi/view/ui/adapter/NewsListAdapter.kt
@@ -3,28 +3,32 @@ package vad.adler.sampleapp.newsapi.view.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import vad.adler.sampleapp.databinding.NewsListItemBinding
 import vad.adler.sampleapp.newsapi.model.NewsServiceResponse
 import vad.adler.sampleapp.newsapi.view.ui.NewsViewModel
-import vad.adler.sampleapp.newsapi.view.ui.adapter.viewholders.NewsListViewHolder
 
-class NewsListAdapter(private val newsListViewModel: NewsViewModel) : RecyclerView.Adapter<NewsListViewHolder>() {
+class NewsListAdapter(private val newsListViewModel: NewsViewModel):
+    RecyclerView.Adapter<NewsListAdapter.NewsListItemsViewHolder>() {
     var newsList: List<NewsServiceResponse.Article> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val dataBinding = NewsListItemBinding.inflate(inflater, parent, false)
-        return NewsListViewHolder(dataBinding, newsListViewModel)
+    class NewsListItemsViewHolder(private val binding: NewsListItemBinding):
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(itemData: NewsServiceResponse.Article) {
+                with(binding) {
+                    Picasso.get().load(itemData.urlToImage).into(newsItemImage);
+                    newsItemHeadline.text = itemData.title.toString()
+                }
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListItemsViewHolder {
+        return NewsListItemsViewHolder(NewsListItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun getItemCount() = newsList.size
 
-    override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
-        holder.setup(newsList[position])
-    }
-
-    fun updateRepoList(newsList: List<NewsServiceResponse.Article>) {
-        this.newsList = newsList
-        notifyDataSetChanged()
+    override fun onBindViewHolder(holder: NewsListItemsViewHolder, position: Int) {
+        holder.bind(newsList[position])
     }
 }
