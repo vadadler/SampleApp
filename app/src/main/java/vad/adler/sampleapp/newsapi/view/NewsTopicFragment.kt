ab0e970a2c172a8239a8b98b4b7be4d4e2e4528d
Vad <vad.adler@gmail.com>
2021-12-09 18:04:15 -0800

    Refactor.

--- app/src/main/java/vad/adler/sampleapp/newsapi/view/NewsTopicFragment.kt
+++ app/src/main/java/vad/adler/sampleapp/newsapi/view/NewsTopicFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import vad.adler.sampleapp.databinding.FragmentNewsListBinding
+import vad.adler.sampleapp.databinding.FragmentNewsTopicBinding
 import vad.adler.sampleapp.newsapi.view.ui.adapter.NewsListAdapter
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,12 +20,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NewsTopicFragment : Fragment() {
+    private lateinit var binding: FragmentNewsTopicBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_topic, container, false)
+//        return inflater.inflate(R.layout.fragment_news_topic, container, false)
+        binding = FragmentNewsTopicBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
